90d099036603bc5a2a155ca4c9c1c809423c93d5
Will Cosgrove <will@willcosgrove.com>
2018-03-06 23:23:33 -0600

    add sidekiq initialization customization

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+redis_conn = -> { Redis.new }
+
+Sidekiq.configure_client do |config|
+  config.redis = ConnectionPool.new(size: 5, &redis_conn)
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = ConnectionPool.new(size: 30, &redis_conn)
+end
