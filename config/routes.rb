0c2d93295f321fe82f13f5957e1cc2b49c188dd4
Will Cosgrove <will@willcosgrove.com>
2018-03-06 23:09:49 -0600

    add sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+require "sidekiq/web"
+
+Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+end if Rails.env.production?
+
 Rails.application.routes.draw do
   get '/retweets/enable',
     to: 'retweet_toggles#create',
@@ -15,6 +22,8 @@ Rails.application.routes.draw do
     req.flash[:alert] = I18n.t(:auth_failure)
     '/'
   }
+
+  mount Sidekiq::Web, at: "/sidekiq"
 
   root to: 'retweet_toggles#index'
 end
